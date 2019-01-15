8ae644824ecba376e27993986f6d148aae35e12d
mahip <mahip18csu118@ncuindia.edu>
2019-01-16 00:02:49 +0530

    Added java file

---
+++ Data1.java
@@ -0,0 +1,20 @@
+class Data1 {
+	public static void main(String args[]){
+	int x=10,y=6,ans;
+	System.out.print("Sum of the above numbers is : ");
+    ans=x+y;
+    System.out.println( + ans);
+    System.out.print("Difference of the above numbers is : ");  
+    ans=x-y;
+    System.out.println( + ans);
+    System.out.print("Multiplication of the above numbers is : ");
+    ans=x*y; 
+    System.out.println( + ans);
+    System.out.print("Division of the above numbers is : ");
+    ans=x/y;
+    System.out.println( + ans);
+    System.out.print("Modulus of the above numbers is : ");
+    ans=x%y;
+    System.out.println( + ans);
+    }
+}
