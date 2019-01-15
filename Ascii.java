29739a66c1e223fa76ff9d2152ddb2a7f4d7ecbb
mahip <mahip18csu118@ncuindia.edu>
2019-01-15 23:19:52 +0530

    added java file

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+class Ascii
+{
+	public static void main(String args[])
+	{
+	char ch='a';
+	int ascii = ch;
+	int castascii = ch;
+	System.out.print("the ascii value of " + ch + " is " +ascii);
+	System.out.print("the ascii value of " + ch + " is " +castascii);
+
+	} 
+}
