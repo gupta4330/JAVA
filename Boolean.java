14264c6edba9a7509f7f63fbce37700652aa0395
mahip <mahip18csu118@ncuindia.edu>
2019-01-17 23:35:32 +0530

    Added java file

---
+++ Boolean.java
@@ -0,0 +1,21 @@
+class Boolean {
+	public static void main(String args[]){
+	int x=13,y=9;
+	boolean b=true,c=false;
+	if(x>y)
+	{
+		System.out.println(b);
+	}
+	else
+		System.out.println(c);
+	
+	if (x==y)
+	{
+		System.out.println("equal");
+	}
+	if (x!=y)
+	{
+		System.out.println("not equal");
+	}
+}
+}
