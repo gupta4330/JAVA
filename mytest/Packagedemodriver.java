a0382ab8adfe0cba09a54f0598e7b05eb68dcee7
gupta4330 <46513313+gupta4330@users.noreply.github.com>
2019-01-31 22:52:07 +0530

    added file

---
+++ mytest/Packagedemodriver.java
@@ -0,0 +1,8 @@
+package exercise.java;
+import exercise.java.*;
+public class Packagedemodriver{
+	public static void main(String[] args) {
+		Packagedemo p1 = new Packagedemo();
+		p1.display();
+	}
+}
