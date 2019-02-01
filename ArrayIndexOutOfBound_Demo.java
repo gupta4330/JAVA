75dbb8bbb5b961d979d0e23cc6fb3f9ea50d5f37
gupta4330 <46513313+gupta4330@users.noreply.github.com>
2019-02-01 19:19:41 +0530

    java program added

---
+++ ArrayIndexOutOfBound_Demo.java
@@ -0,0 +1,12 @@
+class ArrayIndexOutOfBound_Demo { 
+public static void main(String args[]) 
+    { 
+        try { 
+            int a[] = new int[5]; 
+            a[6] = 9;
+        } 
+        catch (ArrayIndexOutOfBoundsException e) { 
+            System.out.println("Array Index is Out Of Bounds"); 
+        } 
+    } 
+} 
